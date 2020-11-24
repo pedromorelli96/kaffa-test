e2c0101e1b5214d1b86de7517c2ce0186a071f9e
Pedro Morelli <pedromorelli96@gmail.com>
2020-11-24 14:41:22 -0300

    Adiciona README.md teste para o primeiro exercicio

---
+++ 1-Validate_CNPJ_format/README.md
@@ -0,0 +1,14 @@
+# 1) Validate CNPJ format (Mask)
+
+### Running the code
+```sh
+$ python3 cnpj_format.py
+```
+or
+```sh
+$ python3 cnpj_format.py < arqX.in
+```
+
+### Screenshot:
+
+![alt text](https://github.com/pedromorelli96/kaffa-test/tree/main/1-Validate_CNPJ_format/run1.png?raw=true)
--- 1-Validate_CNPJ_format/cnpj_format.py
+++ 1-Validate_CNPJ_format/cnpj_format.py
@@ -10,41 +10,28 @@ import re
 
 # Function that receives cnpj string to check if it is
 # a valid format, returns correspondent answer code
-# returns 1 if valid
-# returns 0 if not valid
+# returns True if valid
+# returns False if not valid
 def validate_format(cnpj):
-    answer = 0
+    answer = False
 
     # Primary check : string size
     # if size == 14 : number only 
-    # if size == 18 : formatted
-    # else : incompatible
+    # else : has to be formatted
     if (len(cnpj) == 14):
         # Secondary check : numbers only
         if (cnpj.isdigit()):
-            answer = 1
-            
-    elif (len(cnpj) == 18):
-        # Secondary check :  
-        # numbers and ./- allowed
-        answer = 1
-
-        # Splits the cnpj string using regular expression
-        # Split parameters inside brackets are . / -
-        temp = re.split("[./\\-]+", cnpj)
-
-        # if temp size == 5 then
-        # cnpj has adequate format separators
-        if (len(temp) == 5):
-            # Now to check if all other characters are numbers
-            for section in temp:
-                if not (section.isdigit()):
-                    answer = 0
-        else:
-            answer = 0
+            answer = True
             
     else:
-        answer = 0
+        # Secondary check : correct format
+        pattern = re.compile(r"^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$")
+        # r makes python interpret regex syntax correctly
+        # \d{n} checks for digits n times
+
+        # Check if cnpj matches format
+        if (pattern.match(cnpj)):
+            answer = True
 
     return answer
 
@@ -61,8 +48,3 @@ if (answer):
     print(f"{cnpj} looks like a CNPJ")
 else:
     print(f"{cnpj} does NOT look like a CNPJ")
-
-
-
-
-
