ee3bb85427c12a85968a45004ad14c0b098cfc44
Pedro Morelli <pedromorelli96@gmail.com>
2020-11-26 19:25:31 -0300

    Adiciona arquivos do exercicio 5

---
+++ 5-Simple Todo List/README.md
@@ -0,0 +1,20 @@
+# 5) Simple Todo List
+
+### Python version
+Python 3.8.2
+
+### Running the code
+```sh
+$ python3 todo.py
+```
+
+### Screenshots:
+
+### - First run
+![Test run 1 of exercise 5](run5-1.png)
+
+### - Adding tasks
+![Test run 2 of exercise 5](run5-2.png)
+
+### - Removing tasks
+![Test run 3 of exercise 5](run5-3.png)
--- 5-Simple Todo List/todo.py
+++ 5-Simple Todo List/todo.py
@@ -10,12 +10,19 @@ import json
 # os.path module used to check if json file already exists
 import os.path
 
+# Function check_file_exists is literal
+# returns True if 'filename' in same directory of code exists
 def check_file_exists(filename):
     return os.path.exists(filename)
 
+# Function empty_list checks if list used to store data is empty
+# returns True if 'todolist' is empty
 def empty_list(todolist):
     return not todolist
 
+# Function add_task asks user for task input on stdin
+# then adds task string to 'todolist' and shows a
+# completion message on stdout
 def add_task(todolist):
     print("-"*5)
     print("Enter your task below:")
@@ -23,6 +30,10 @@ def add_task(todolist):
     todolist.append(task)
     print(f"Task #{len(todolist)} added!")
 
+# Function remove_task asks user for task number on stdin
+# then removes task based on position from 'todolist' and shows
+# a completion message on stdout if successful, otherwise
+# warns user about incorrect task number entered
 def remove_task(todolist):
     print("-"*5)
     print("Which task (number) do you want to remove?")
@@ -33,6 +44,15 @@ def remove_task(todolist):
     else:
         print(f"Task {task} does NOT exist!")
 
+# Function remove_all_tasks removes all tasks from 'todolist'
+# returns a successful message to user on stdout
+def remove_all_tasks(todolist):
+    print("-"*5)
+    for i in range(len(todolist)):
+        todolist.pop(i)
+    print("All tasks deleted.")
+
+# Function print_todolist shows all tasks (if any) on stdout
 def print_todolist(todolist):
     print("####### TODO LIST #######")
     if (empty_list(todolist)):
@@ -45,13 +65,19 @@ def print_todolist(todolist):
             i += 1
         print("#"*25)
 
+# Function task_request_msg prints on stdout the
+# possible command inputs for todo list control
 def task_request_msg():
     print("Enter:")
     print("1 - Add Task")
     print("2 - Remove Task")
     print("3 - Show Todo List")
+    print("4 - Delete all Tasks")
     print("q - Quit Application")
 
+# Function parse_input checks parameter 'keybinput'
+# and picks the correct method acording to its code
+# otherwise, warns user about incorrect input
 def parse_input(keybinput, todolist):
     if (keybinput == '1'):
         add_task(todolist)
@@ -61,11 +87,21 @@ def parse_input(keybinput, todolist):
         print_todolist(todolist)
     elif (keybinput == '3'):
         print_todolist(todolist)
+    elif (keybinput == '4'):
+        remove_all_tasks(todolist)
     elif (keybinput == 'q'):
         print("Exiting Todo List...")
     else:
         print("Invalid input, try again!")
 
+# Function manage_todolist is the main function of the program
+# First, it checks if the file we use already exists and reads
+# from it if it does, otherwise we create an empty list
+# Second, it shows all tasks (if any) in current todo list
+# Third, reads from user input and calls correspondent methods
+# to control the list and its tasks
+# Finally, it saves on the 'filename' json file the list of tasks
+# and closes the file
 def manage_todolist():
 
     filename = 'todo.json'
